be208a8aa1475cd905f09bc7a9c9b911520a4e1b
Ylva <ylva.os@uit.no>
2026-07-09 15:09:47 +0200

    update and restructuring

---
+++ runscripts/betzy/betzy_mrchem.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -A nn14654k
+#SBATCH --nodes=8
+#SBATCH --tasks-per-node=6
+#SBATCH --exclusive
+#SBATCH --time=20:00:0
+
+source /cluster/projects/nn14654k/ylvaos/mrchem/tools/betzy.env
+
+export UCX_LOG_LEVEL=ERROR
+
+inp="TPhP"
+dir="geomopt_wb97x-d4_bp86_1e6"
+
+cd mrchem/$dir
+
+/cluster/projects/nn14654k/ylvaos/mrchem/install/bin/mrchem -D $inp.inp
+mpirun --rank-by node -map-by socket -bind-to socket /usr/bin/time -f %M /cluster/projects/nn14654k/ylvaos/mrchem/install/bin/mrchem.x $inp.json > $inp.out
+
+rm -rf checkpoint
+rm -rf orbitals
+rm -rf plots
