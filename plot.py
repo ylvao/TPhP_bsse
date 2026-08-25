fe4eff6e91c05e36452fedeb4b6eb7477c9554d3
ylvao <ylva.os@uit.no>
2026-08-25 17:31:38 +0200

    update data

--- plot.py
+++ plot.py
@@ -1,24 +1,148 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-mrchem = [-8.560929421702415]
+to_kcal = 627.15
 
-# svp, tzvp, qzvp kcal/mol
-orca = np.array([-16.917, -13.905, -13.266])
-orca_corr = np.array([-10.729254780449935, -12.550824019125006, -12.979818359730594])
-orca_bsse = np.array([-6.188239530899327, -1.3545927723571072, -0.2863064420189312])
 
-xs = [0, 1 , 2]
+def calc_bsse(AB, A, B, AAB, AA, BAB, BB):
+    return (AB - A - B - AAB + AA - BAB + BB)
 
-plt.plot(xs, orca, label = "orca no bsse")
-plt.plot(xs, orca_corr, label = "orca bsse corrected")
-# plt.plot(xs, orca_bsse, label = "bsse")
+def dE(A, B, AB):
+    return AB - A - B
+
+# hartree
+data = {
+    "pph3": {
+        "mrchem" : [
+            -7.556679444023e+03, # 1b (AB)
+            -6.520018498931e+03, # 15h (A)
+            -1.036647302394e+03, # PPh3 (B)
+        ],
+        "def2svp" : [
+            -2168.905161567229, # A
+            -1035.725629100780, # B
+            -3204.657750388669, # AB
+            -2168.872606823358, # AA
+            -1035.724249855712, # BB
+            -2168.876610161623, # AAB
+            -1035.730108096595, # BAB
+        ],
+        "def2svpd" : [
+            -2168.979286305878,
+            -1035.760278527500,
+            -3204.779893188574,
+            -2168.951580440749,
+            -1035.759104140504,
+            -2168.959551014362,
+            0,
+        ],
+        "def2tzvp" : [
+            -2170.655315618085, # A
+            -1036.584001955480, # B
+            -3207.261477247182, # AB
+            -2170.621422171998, # AA
+            -1036.582817045620, # BB
+            -2170.622650691153,
+            -1036.583747205645,
+        ],
+        "def2tzvpd" : [
+            -2170.661658972940,
+            -1036.586641118505,
+            -3207.271069620780,
+            -2170.627903935866,
+            -1036.585442863314,
+            -2170.629218925415,
+            0,
+        ],
+        "def2qzvp" : [
+            -2170.782588960151,
+            -1036.644046685794,
+            -3207.447776543782,
+            -2170.748208017691,
+            -1036.642859121321,
+            -2170.748474197015,
+            -1036.643049199972,
+        ]
+    },
+    "ph3": {
+        "mrchem" : [
+            -5.476343590773e+03,
+            -5.133127519611e+03,
+            -3.432051284664e+02,
+        ],
+        "def2tzvp": [
+            -783.896515368581,
+            -343.205210725975,
+            -1127.117806365128,
+            -783.834198107267,
+            -343.204238930680,
+            -783.834307927292,
+            -343.204273401243,
+        ]
+        },
+    "pme3": {
+        "mrchem" : [
+            -5830.418959220,
+            -5369.162853394,
+            -461.2137003838,
+        ],
+        "def2tzvp": [
+
+        ]
+        },
+    "pet3": {
+        "mrchem" : [
+            -6.184324438986e+03,
+            -5.605108965257e+03,
+            -5.791918249236e+02,
+        ],
+        "def2tzvp": [
+
+        ]
+        },
+    "ptbu3": {
+        "mrchem" : [
+
+        ],
+        "def2tzvp": [
+
+        ]
+        },
+}
+
+ligand = "pph3"
+basis_sets = ["def2svp", "def2tzvp", "def2qzvp"]
+basis_sets2 = ["def2svpd", "def2tzvpd"]
+
+mrchem = (dE(data[ligand]["mrchem"][1], data[ligand]["mrchem"][2], data[ligand]["mrchem"][0]) * to_kcal)
+orca_no_bsse = []
+orca_no_bsse2 = []
+orca_cp_corr = []
+orca_cp_corr2 = []
+for basis in basis_sets:
+    # Same order as the orca bsse input file
+    A, B, AB, AA, BB, AAB, BAB = data[ligand][basis]
+    orca_no_bsse.append(dE(A, B, AB) * to_kcal)
+    orca_cp_corr.append(calc_bsse(AB, A, B, AAB, AA, BAB, BB) * to_kcal)
+for basis in basis_sets2:
+    # Same order as the orca bsse input file
+    A, B, AB, AA, BB, AAB, BAB = data[ligand][basis]
+    orca_no_bsse2.append(dE(A, B, AB) * to_kcal)
+    orca_cp_corr2.append(calc_bsse(AB, A, B, AAB, AA, BAB, BB) * to_kcal)
+
+print(mrchem)
+print(orca_no_bsse)
+print(orca_cp_corr)
+xs = list(range(len(basis_sets)))
+xs2 = list(range(len(basis_sets2)))
+
+plt.plot(xs, orca_no_bsse, label = "orca no bsse")
+plt.plot(xs2, orca_no_bsse2, label = "orca no bsse aug")
+# plt.plot(xs, orca_cp_corr, label = "orca bb-cp corrected")
 plt.plot(xs, [mrchem for _ in range(len(xs))], label = "mrchem")
+
 
 plt.legend()
 plt.savefig("plot.png")
 
 
-n = -3204.657750388669 - -2168.905161567229 - -1035.725629100780 - -2168.876610161623 + -2168.872606823358 - -1035.730108096595 + -1035.724249855712 
-print(n * 627.51)
-print(bsse[0])
--- runscripts/olivia/olivia_orca.sh
+++ runscripts/olivia/olivia_orca.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=%j.err
 
 
-#SBATCH --job-name=bbcp_pet3
+#SBATCH --job-name=bbcp_qzvpd
 #SBATCH --time=0-20:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
@@ -20,11 +20,11 @@ module load OpenMPI/5.0.9-GCC-14.3.0
 export CMAKE_TLS_VERIFY=0
 
 # dir="orca/opt1_bp86_ptbu3/for_mrchem_init_guess"
-dir="orca/opt1_bp86_pet3/bbcp"
+dir="orca/opt1_bp86/bbcp"
 # dir="orca/opt1_bp86_ptbu3"
 # dir="orca/geomopt_wb97x-d4_def2svp_ptbu3"
 # name="15h_ptbu3_opt"
-name="bbcp_def2qzvp"
+name="bbcp_def2qzvpd"
 # name="x2csvp_ptbu3"
 # name="bbcp_def2qzvp"
 
